557082b7267dda7febb8d03b6d81d208c47178df
calin <calindotgabriel18@gmail.com>
2016-12-04 14:08:46 +0200

    ADDITION WORKS

--- src/main/java/com/motanttron/MatrixMultiAdditionThread.java
+++ src/main/java/com/motanttron/MatrixMultiAdditionThread.java
@@ -3,12 +3,29 @@ package com.motanttron;
 /**
  * Created by calin on 04.12.2016.
  */
-public class MatrixMultiAdditionThread {
+public class MatrixMultiAdditionThread implements Runnable{
+    private Position p1;
+    private Position p2;
+    private Matrix m1;
+    private Matrix m2;
+    private Matrix m3;
+
     public MatrixMultiAdditionThread(Position p1, Position p2, Matrix m1, Matrix m2, Matrix m3) {
         // should verify arguments
+        this.p1 = p1;
+        this.p2 = p2;
+        this.m1 = m1;
+        this.m2 = m2;
+        this.m3 = m3;
+    }
+
+    @Override
+    public void run() {
+        final long id = Thread.currentThread().getId();
         for (int i = p1.x; i <= p2.x; i++) {
             for (int j = p1.y; j <= p2.y; j++) {
-                System.out.printf("Should add [%d,%d]\n", i, j);
+                System.out.printf("Thread {%d} should add [%d,%d]\n", id, i, j);
+                m3.set(i, j, m1.get(i, j) + m2.get(i, j));
             }
         }
     }
--- src/test/java/com/motanttron/Test103.java
+++ src/test/java/com/motanttron/Test103.java
@@ -4,6 +4,11 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by calin on 04.12.2016.
@@ -17,13 +22,27 @@ public class Test103 {
      */
     @Test
     public void test() {
-        final Matrix m1 = MatrixGenerator.generateIncremental(4, 4);
-        final Matrix m2 = MatrixGenerator.generateIncremental(4, 4);
-        final Matrix m3 = MatrixGenerator.generateEmpty(4, 4);
+        final int rows = 50;
+        final int cols = 50;
+        final int threads = 3;
+        final Matrix m1 = MatrixGenerator.generateIncremental(rows, cols);
+        final Matrix m2 = MatrixGenerator.generateIncremental(rows, cols);
+        final Matrix m3 = MatrixGenerator.generateEmpty(rows, cols);
         m1.showIndices();
-        Position p1 = new Position(0, 0);
-        Position p2 = new Position(2, 0);
-        final MatrixMultiAdditionThread t = new MatrixMultiAdditionThread(p1, p2, m1, m2, m3);
+        final List<Section> sections = getSections(rows, cols, threads);
+
+        final ExecutorService executor = Executors.newFixedThreadPool(threads);
+        for (Section section : sections) {
+            executor.submit(new MatrixMultiAdditionThread(section.from, section.to, m1, m2, m3));
+        }
+        executor.shutdown();
+        try {
+            executor.awaitTermination(1, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        m3.show();
     }
 
     /**
@@ -41,20 +60,21 @@ public class Test103 {
      * If no rest, the balancing is equal amongst threads.
      * Start from [0, 0] and then advance by the ratio on the rows.
      * For the first case, the next position will be [2, 0]
+     * If rest, the value of rows % threads is actually how many rows are left "behind" at the end
      */
     /**
      * Start from (0, 0), For next, add by x { ratio - 1 } and by y { cols - 1 }and you get the first section
      * Add to start the ratio by x and to next increment the x by ratio
      * For a 4 x 4 matrix and 2 threads it should look like:
-     *  [ ([0,0], [1, 3]), ([2,0), (3,3)) ]
-     *
+     * [ ([0,0], [1, 3]), ([2,0), (3,3)) ]
+     * <p>
      * (0, 0), (1, 3) ok (2, 0), (3, 3) ok
      * startPoint = 0, 0
      * endPoint = 1, 3
      * startPoint = 2, 0
      * endPoint = 3, 3
      * startPoint = 4, 0 STOP because 4 >= numberOfRows
-     *
+     * <p>
      * 6 x 6 matrix by 3 threads => r = 3
      * startPoint = 0, 0
      * endPoint = 1, 5
@@ -65,24 +85,46 @@ public class Test103 {
      */
     @Test
     public void test2() {
-        final Matrix m = MatrixGenerator.generateIncremental(4, 4);
-        int threads = 4;
+        final Matrix m = MatrixGenerator.generateIncremental(6, 6);
+        m.showIndices();
+        int threads = 2;
+        System.out.println("threads = " + threads);
+
+        final List<Section> sections = getSections(m, threads);
+
+        for (Section section : sections) {
+            System.out.println(section);
+        }
+        System.out.println();
+    }
+
+    private List<Section> getSections(Matrix m, int threads) {
         final int rows = m.getRows();
         final int cols = m.getCols();
-        final int ratio = rows / threads; // 2
+        return getSections(threads, rows, cols);
+    }
+
+    private List<Section> getSections(int rows, int cols, int threads) {
+        final int ratio = rows / threads;
+        System.out.println("ratio = " + ratio);
+        final int rest = rows % threads;
+        System.out.println("rest = " + rest);
 
         List<Section> sections = new ArrayList<>();
         Position start = new Position(0, 0);
         Position end = new Position(ratio - 1, cols - 1);
         sections.add(new Section(start, end));
 
-        for (int i = 0 ; i < threads - 1; i ++) {
+        for (int i = 0; i < threads - 1; i++) {
             start = start.advanceX(ratio);
             end = end.advanceX(ratio);
             sections.add(new Section(start, end));
         }
+        if (rest != 0) {
+            final Section lastSection = sections.get(sections.size() - 1);
+            lastSection.to.x += rest;
+        }
 
-        System.out.println();
-
+        return sections;
     }
 }
